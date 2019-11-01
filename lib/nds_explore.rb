9ab15ae62ca2c5976208e673e9d3aae85849c25a
declarative-blog-8005 <anna@annajcarey.com>
2019-11-01 18:40:40 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,9 @@ end
 def print_first_directors_movie_titles
   nds = directors_database
   index = 0
-  puts nds[0][:movies]
-  puts "length is" 
-  puts nds[0][:movies].length
+  #puts nds[0][:movies]
+  #puts "length is" 
+  while index < nds[0][:movies].length
+    puts nds[0[:movies][:title]
+    index +=1 
 end
